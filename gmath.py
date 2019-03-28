011e448261c75745d4f15fac40ecd33e1d4e2642
Alexia Leong <aleong1@stuy.edu>
2019-03-28 11:25:59 -0400

    wow

--- gmath.py
+++ gmath.py
@@ -9,11 +9,27 @@ def normalize(vector):
     x = vector[0]
     y = vector[1]
     z = vector[2]
-    mag = math.sqrt(x*x+y*y=z*z)
+    mag = math.sqrt(x*x+y*y+z*z)
+
+    vector[0] = x/mag
+    vector[1] = y/mag
+    vector[2] = z/mag
     
 
-#Return the dot porduct of a . b
+#Return the dot product of a . b
 def dot_product(a, b):
+    x0 = a[0]
+    y0 = a[1]
+    z0 = a[2]
+    magA = math.sqrt(x0*x0+y0*y0+z0*z0)
+
+    x1 = a[0]
+    y1 = a[1]
+    z1 = a[2]
+    magB = math.sqrt(x1*x1+y1*y1+z1*z1)
+
+    dp = magA * magB * math.cos()
+    
     return 0
 
 #Calculate the surface normal for the triangle whose first
